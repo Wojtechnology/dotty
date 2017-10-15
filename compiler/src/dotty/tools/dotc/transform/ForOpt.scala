64ba5db5b263de46d1a4070e6719bc90eaf8bab5
Wojtek Swiderski <wswiders@uwaterloo.ca>
2017-10-15 10:43:04 -0400

    Code copying - small fixes

--- compiler/src/dotty/tools/dotc/transform/ForOpt.scala
+++ compiler/src/dotty/tools/dotc/transform/ForOpt.scala
@@ -96,31 +96,34 @@ object ForOpt {
       )
     }
 
-    private def copyMethod(oldSym: Symbol, copySym: TermSymbol, rhs: Tree)(implicit ctx: Context, info: TransformerInfo): DefDef = {
-      // changeOwner needed for things like defDef, etc
-      // subst needed for the Return value, although I might want to actually transform the
-      // Return statement instead since it seems like the name of that symbol is still `foreach`
-      val tree = rhs.changeOwner(oldSym, copySym).subst(oldSym :: Nil, copySym :: Nil)
+    private def copyMethod(oldTree: DefDef, copySym: TermSymbol)(implicit ctx: Context, info: TransformerInfo): DefDef = {
+      val oldSym = oldTree.symbol
+
+      // changeOwner needed for things like defDef, ident, etc
+      // Might also want to run subst, but works without it for now. It was my previous solution
+      // to changing the Return statement below
+      val rhs = oldTree.rhs.changeOwner(oldSym, copySym)
 
       polyDefDef(
         copySym,
         ttypes =>
-          vparamss =>
+          vparamss => {
             // For some reason, changing the owner is not enough, we also have to replace any references
             // to parameters with the ones given to us above.
+            // TODO: may also need to do this for ttypes (although probably not, since types can be copied (?))
+            val vparamssMap =
+              oldTree.vparamss.flatten.map(_.symbol).zip(vparamss.flatten.map(_.symbol)).toMap
             new TreeTypeMap(treeMap = {
-              case d: Ident =>
-                val thing = vparamss.flatten.find({
-                  case vparam: Ident =>
-                    vparam.name == d.name // TODO: find more legit way of doing this...
-                  case bro => false
-                })
-                thing match {
-                  case Some(other) => ref(other.symbol)
-                  case None => d
+              case ident: Ident =>
+                vparamssMap.get(ident.symbol) match {
+                  case Some(newVparam) => ref(newVparam)
+                  case None => ident
                 }
-              case tree => tree
-            }).transform(tree)
+              case rtn: Return =>
+                Return(rtn.expr, ref(copySym))
+              case other => other
+            }).transform(rhs)
+          }
       )
     }
 
@@ -130,9 +133,7 @@ object ForOpt {
       if (classes contains cls) {
         val body = tree.body.flatMap {
           case subTree: DefDef if methods contains subTree.symbol =>
-            subTree :: methToProxies(subTree.symbol).map(copyMethod(subTree.symbol, _, subTree.rhs)).toList
-            // println(subTree)
-            // subTree :: Nil
+            subTree :: methToProxies(subTree.symbol).map(copyMethod(subTree, _)).toList
           case subTree => subTree :: Nil
         }
         cpy.Template(tree)(body = body)
