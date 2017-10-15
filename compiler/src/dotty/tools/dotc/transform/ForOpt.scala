5351225d2d4b61e1776f9ebc26639eacf6b6c5d3
Wojtek Swiderski <wswiders@uwaterloo.ca>
2017-10-15 00:11:20 -0400

    Code copying

--- compiler/src/dotty/tools/dotc/transform/ForOpt.scala
+++ compiler/src/dotty/tools/dotc/transform/ForOpt.scala
@@ -1,9 +1,11 @@
 package dotty.tools.dotc
 
-import dotty.tools.dotc.ast.tpd
+import dotty.tools.dotc.ast.{TreeTypeMap, tpd}
 import dotty.tools.dotc.core.Contexts._
 import dotty.tools.dotc.core.Flags._
 import dotty.tools.dotc.core.Symbols._
+import dotty.tools.dotc.core.Types._
+import dotty.tools.dotc.transform.SymUtils._
 import dotty.tools.dotc.transform.TreeTransforms.{MiniPhaseTransform, TransformerInfo}
 
 import scala.collection.immutable
@@ -17,7 +19,8 @@ object ForOpt {
 
   // List of classes that contain methods in `methods`. Only templates of this class will have
   // proxy methods rewritten.
-  private def classes(implicit ctx: Context) = immutable.Set(defn.RangeClass)
+  private def classes(implicit ctx: Context): immutable.Set[ClassSymbol] =
+    immutable.Set(defn.RangeClass)
 
   // List of methods for which we want to create proxy methods
   private def methods(implicit ctx: Context) = immutable.Set(defn.Range_foreach)
@@ -50,24 +53,35 @@ object ForOpt {
       proxySym
     }
 
-    override def transformApply(tree: Apply)(implicit ctx: Context, info: TransformerInfo): Tree = {
-      val sym = tree.symbol
-      if (methods contains sym) {
-        def buildApply(tree: Tree): Tree = tree match {
-          case tApply: TypeApply => buildApply(tApply.fun).appliedToTypeTrees(tApply.args)
-          // TODO: Is it possible for this method to be returned from another method?
-          case apply: Apply => buildApply(apply.fun).appliedToArgs(apply.args)
-          case select: Select => select.qualifier.select(buildProxySymbol(sym))
-          // TODO: Figure out Ident case, when necessary
-          case _ =>
-            assert(false, s"Invalid tree of type ${tree.getClass}")
-            EmptyTree
-        }
-
-        buildApply(tree)
-      } else tree
+    override def transformSelect(tree: Select)(implicit ctx: Context, info: TransformerInfo): Tree = {
+      if (methods contains tree.symbol) tree.qualifier.select(buildProxySymbol(tree.symbol))
+      else tree
     }
 
+  }
+
+  class EnclosureTransform(newOwner: Symbol) extends MiniPhaseTransform {
+    override def phaseName: String = "enclosureTransform"
+
+    override def transformDefDef(tree: DefDef)(implicit ctx: Context, info: TransformerInfo): Tree = {
+      println(tree)
+      val oldSym = tree.symbol
+      val newSym = ctx.newSymbol(
+        newOwner,
+        oldSym.name.asTermName,
+        oldSym.flags,
+        oldSym.info,
+        coord = oldSym.pos
+      )
+      DefDef(newSym, tree.rhs)
+    }
+
+    override def transformIdent(tree: Ident)(implicit ctx: Context, info: TransformerInfo): Tree = {
+
+      val tr = Ident(TermRef(NoPrefix, tree.symbol.asTerm))
+      println(tr)
+      tr
+    }
   }
 
   class ForOptTransform extends MiniPhaseTransform {
@@ -82,15 +96,43 @@ object ForOpt {
       )
     }
 
+    private def copyMethod(oldSym: Symbol, copySym: TermSymbol, rhs: Tree)(implicit ctx: Context, info: TransformerInfo): DefDef = {
+      // changeOwner needed for things like defDef, etc
+      // subst needed for the Return value, although I might want to actually transform the
+      // Return statement instead since it seems like the name of that symbol is still `foreach`
+      val tree = rhs.changeOwner(oldSym, copySym).subst(oldSym :: Nil, copySym :: Nil)
+
+      polyDefDef(
+        copySym,
+        ttypes =>
+          vparamss =>
+            // For some reason, changing the owner is not enough, we also have to replace any references
+            // to parameters with the ones given to us above.
+            new TreeTypeMap(treeMap = {
+              case d: Ident =>
+                val thing = vparamss.flatten.find({
+                  case vparam: Ident =>
+                    vparam.name == d.name // TODO: find more legit way of doing this...
+                  case bro => false
+                })
+                thing match {
+                  case Some(other) => ref(other.symbol)
+                  case None => d
+                }
+              case tree => tree
+            }).transform(tree)
+      )
+    }
+
     override def transformTemplate(tree: Template)(implicit ctx: Context,
                                                    info: TransformerInfo): Tree = {
       val cls = ctx.owner.asClass
-
-      if (cls == defn.RangeClass) {
+      if (classes contains cls) {
         val body = tree.body.flatMap {
           case subTree: DefDef if methods contains subTree.symbol =>
-            methToProxies(subTree.symbol).map(
-              proxyMethod(subTree.symbol, _)).toList ::: subTree :: Nil
+            subTree :: methToProxies(subTree.symbol).map(copyMethod(subTree.symbol, _, subTree.rhs)).toList
+            // println(subTree)
+            // subTree :: Nil
           case subTree => subTree :: Nil
         }
         cpy.Template(tree)(body = body)
